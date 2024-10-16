73e6f169e895dd5f00decd7370bdeb29aea19692
Esteban Regueira <regueira.esteban@inta.gob.ar>
2024-10-16 13:39:07 -0300

    first commit

---
+++ resources/js/index.js
@@ -0,0 +1,33 @@
+const abecedario = 'abcdefghijklmnñopqrstuvwxyz';
+
+function encriptar() {
+    const texto = document.getElementById('texto').value;
+    let resultado = '';
+    for (let i = 0; i < texto.length; i++) {
+        const caracter = texto[i].toLowerCase();
+        const indice = abecedario.indexOf(caracter)+1;
+        if (indice !== -1) {
+            const nuevoIndice = (indice + 3) % abecedario.length;
+            resultado += abecedario[nuevoIndice];
+        } else {
+            resultado += caracter;
+        }
+    }
+    document.getElementById('resultado').innerText = 'Resultado: ' + resultado;
+}
+
+function desencriptar() {
+    const texto = document.getElementById('texto').value;
+    let resultado = '';
+    for (let i = 0; i < texto.length; i++) {
+        const caracter = texto[i].toLowerCase();
+        const indice = abecedario.indexOf(caracter)+1;
+        if (indice !== -1) {
+            const nuevoIndice = (indice - 3 + abecedario.length) % abecedario.length;
+            resultado += abecedario[nuevoIndice];
+        } else {
+            resultado += caracter;
+        }
+    }
+    document.getElementById('resultado').innerText = 'Resultado: ' + resultado;
+}
